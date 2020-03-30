702b4614edb3bd79a3be0765734453201f002cb3
Tyler Menezes <tylermenezes@gmail.com>
2020-03-30 18:36:19 -0400

    Set environment variable IMAGE_TAG upon deploy

--- pages/api/[jobId]/[groupName]/[taskName]/update-tag.js
+++ pages/api/[jobId]/[groupName]/[taskName]/update-tag.js
@@ -1,23 +1,34 @@
-import { getJob, updateJob } from '../../../../../utils/nomad'
+import { getJob, updateJob } from "../../../../../utils/nomad";
+
+const splitImage = image => {
+  const lastIndex = image.lastIndexOf(":");
+  const repo = image.substr(0, lastIndex);
+  const tag = image.substr(lastIndex + 1) || "latest";
+  return [repo, tag];
+};
 
 export default async (req, res) => {
   const { tag } = JSON.parse(req.body);
   const { jobId, groupName, taskName } = req.query;
   const jobSpec = await getJob(jobId);
 
-  jobSpec.TaskGroups.map((group) => {
+  jobSpec.TaskGroups.map(group => {
     if (group.Name !== groupName) return group;
 
-    group.Tasks = group.Tasks.map((task) => {
+    group.Tasks = group.Tasks.map(task => {
       if (task.Name !== taskName) return task;
 
-      const [ repo ] = task.Config.image.split(':', 2);
+      const [repo] = splitImage(task.Config.image);
       task.Config.image = `${repo}:${tag}`;
+
+      if (!task.Env) task.Env = {};
+      task.Env.IMAGE_TAG = tag;
+
       return task;
-    })
+    });
 
     return group;
   });
 
   res.send(await updateJob(jobId, jobSpec));
-}
+};
