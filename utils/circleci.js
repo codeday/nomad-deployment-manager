de87d1501cfef8a21a51c57f82cd42050f7729ca
Tyler Menezes <tylermenezes@gmail.com>
2020-03-30 18:19:09 -0400

    Fix multiple builds showing up

--- utils/circleci.js
+++ utils/circleci.js
@@ -1,14 +1,16 @@
-require('dotenv').config();
-import request from 'request-promise-native'
+require("dotenv").config();
+import request from "request-promise-native";
 
-export const getBuildsForProject = async (repo) => {
+export const getBuildsForProject = async repo => {
   const url = `https://circleci.com/api/v1.1/project/gh/${repo}/tree/master?limit=20&filter=successful&circle-token=${process.env.CIRCLECI_TOKEN}`;
-  const builds = JSON.parse(await request(url, { headers: { 'accept': 'application/json' }}));
+  const builds = JSON.parse(
+    await request(url, { headers: { accept: "application/json" } })
+  );
   return builds
-    .filter((build) => build.build_parameters.CIRCLE_JOB === 'build' || (build.workflows && build.workflows.workflow_name === 'build'))
-    .map((cBuild) => ({
+    .filter(build => build.build_parameters.CIRCLE_JOB === "build")
+    .map(cBuild => ({
       id: cBuild.vcs_revision,
       committer: cBuild.committer_name,
-      message: cBuild.subject,
+      message: cBuild.subject
     }));
-}
+};
