f0a79a8486326719b9e24a88dc7f6638820074e6
Tyler Menezes <github@xanadu.ws>
2020-10-09 14:09:16 -0400

    Check for main and master branches

--- utils/circleci.js
+++ utils/circleci.js
@@ -2,10 +2,18 @@ require("dotenv").config();
 import request from "request-promise-native";
 
 export const getBuildsForProject = async repo => {
-  const url = `https://circleci.com/api/v1.1/project/gh/${repo}/tree/master?limit=20&filter=successful&circle-token=${process.env.CIRCLECI_TOKEN}`;
-  const builds = JSON.parse(
+  const url = `https://circleci.com/api/v1.1/project/gh/${repo}/tree/main?limit=20&filter=successful&circle-token=${process.env.CIRCLECI_TOKEN}`;
+  const urlOld = `https://circleci.com/api/v1.1/project/gh/${repo}/tree/master?limit=20&filter=successful&circle-token=${process.env.CIRCLECI_TOKEN}`;
+
+  let builds = JSON.parse(
     await request(url, { headers: { accept: "application/json" } })
   );
+
+  if (!builds || builds.length === 0) {
+    builds = JSON.parse(
+      await request(urlOld, { headers: { accept: "application/json" } })
+    );
+  }
   return builds
     .filter(build => build.build_parameters.CIRCLE_JOB === "build")
     .map(cBuild => ({
